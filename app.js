f46fe2a3a8b2d4c39868550300b98751ff60e848
evanst1 <tylere463@gmail.com>
2023-02-09 14:34:26 -0500

    buttons

---
+++ app.js
@@ -0,0 +1,7 @@
+function increaseFontSizeBy1px() {
+    txt = document.getElementById('b');
+    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
+    currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize + 1) + 'px';
+}
+
